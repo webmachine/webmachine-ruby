4fc1839c3c09ee3a8670c0b1b123fc41a96b6a3a
Sean Cribbs <sean@basho.com>
2011-08-14 20:38:21 -0400

    Refactor conneg to encapsulate more logic into objects.

--- lib/webmachine/decision/conneg.rb
+++ lib/webmachine/decision/conneg.rb
@@ -12,24 +12,16 @@ module Webmachine
       # appropriate media type.
       # @api private
       def choose_media_type(provided, header)
-        requested = header.split(/\s*,\s*/).inject(MediaTypeList.new) do |acc, v|
-          acc.add_header_val(v)
-          acc
-        end
+        requested = MediaTypeList.build(header.split(/\s*,\s*/))
         provided = provided.map do |p| # normalize_provided
-          case p
-          when String
-            [p, {}]
-          when Array
-            p
-          end
+          MediaType.new(*Array(p))
         end
         # choose_media_type1
         chosen = nil
-        requested.each do |_, type_and_params|
-          break if chosen = media_match(type_and_params, provided)
+        requested.each do |_, requested_type|
+          break if chosen = media_match(requested_type, provided)
         end
-        format_content_type(*chosen) if chosen
+        chosen.to_s if chosen
       end
 
       # Given the 'Accept-Encoding' header and provided encodings, chooses an appropriate
@@ -57,10 +49,12 @@ module Webmachine
         end
       end
 
-      # Makes an conneg choice based what is accepted and what is provided.
+      # Makes an conneg choice based what is accepted and what is
+      # provided.
+      # @api private
       def do_choose(choices, header, default)
         choices = choices.dup.map {|s| s.downcase }
-        accepted = build_conneg_list(header.split(/\s*,\s/))
+        accepted = PriorityList.build(header.split(/\s*,\s/))
         default_priority = accepted.priority_of(default)
         star_priority = accepted.priority_of("*")
         default_ok = (default_priority.nil? && star_priority != 0.0) || default_priority
@@ -78,36 +72,67 @@ module Webmachine
           (default_ok && choices.include?(default) && default) # Or default
       end
 
-      # Given the choices in a conneg header, order them by appearance
-      # but also accounting for priority given by the client in the
-      # 'q' parameter.
-      def build_conneg_list(choices)
-        choices.inject(PriorityList.new) do |acc, c|
-          acc.add_header_val(c)
-          acc
-        end
-      end
-
       private
-      def format_content_type(type, params)
-        [type, *params.map {|k,v| "#{k}=#{v}" }].join(";")
-      end
 
-      def media_match(requested, provided)
-        rtype, rparams = requested
-        return provided.first if rtype == "*/*" && rparams.empty?
-        provided.find do |(type, params)|
-          media_type_match(type, rtype) && params == rparams # media_params_match
+      # Encapsulates a MIME media type, with logic for matching types.
+      class MediaType
+        # @return [String] the MIME media type
+        attr_accessor :type
+
+        # @return [Hash] any type parameters, e.g. charset
+        attr_accessor :params
+
+        def initialize(type, params={})
+          @type, @params = type, params
+        end
+
+        # Detects whether the {MediaType} represents an open wildcard
+        # type, that is, "*/*" without any {#params}.
+        def matches_all?
+          @type == "*/*" && @params.empty?
+        end
+
+        # Detects whether this {MediaType} matches the other {MediaType},
+        # taking into account wildcards.
+        def match?(other)
+          type_matches?(other) && other.params == params
+        end
+
+        # Reconstitutes the type into a String
+        def to_s
+          [type, *params.map {|k,v| "#{k}=#{v}" }].join(";")
+        end
+
+        # @return [String] The major type, e.g. "application", "text", "image"
+        def major
+          type.split("/").first
+        end
+
+        # @return [String] the minor or sub-type, e.g. "json", "html", "jpeg"
+        def minor
+          type.split("/").last
+        end
+
+        private
+        def type_matches?(other)
+          if ["*", "*/*", type].include?(other.type)
+            true
+          else
+            other.major == major && other.minor == "*"
+          end
         end
       end
 
-      def media_type_match(requested, provided)
-        if ["*", "*/*", provided].include?(requested)
-          true
-        else
-          r1, r2 = requested.split('/')
-          p1, _ = provided.split('/')
-          r2 == "*" && r1 == p1
+      # Matches the requested media type (with potential modifiers)
+      # against the provided types (with potential modifiers).
+      # @param [MediaType] requested the requested media type
+      # @param [Array<MediaType>] provided the provided media
+      #     types
+      # @return [MediaType] the first media type that matches
+      def media_match(requested, provided)
+        return provided.first if requested.matches_all?
+        provided.find do |p|
+          p.match?(requested)
         end
       end
 
@@ -126,26 +151,42 @@ module Webmachine
       end
 
       # @private
+      # Content-negotiation priority list that takes into account both
+      # assigned priority ("q" value) as well as order, since items
+      # that come earlier in an acceptance list have higher priority
+      # by fiat.
       class PriorityList
-        include Enumerable
-        # This needs to consider order as well as priority since an item
-        # that comes earlier in the header has higher priority by
-        # fiat, so we can't just sort by priority. We have to group by
-        # priority, in order, then sort the groups by priority, and
-        # finally concatenate.
+        # Given an acceptance list, create a PriorityList from them.
+        def self.build(list)
+          new.tap do |plist|
+            list.each {|item| plist.add_header_val(item) }
+          end
+        end
 
+        include Enumerable
+
+        # Matches acceptable items that include 'q' values
         CONNEG_REGEX = /^\s*(\S+);\s*q=(\S*)\s*$/
 
+        # Creates a {PriorityList}.
+        # @see PriorityList::build
         def initialize
           @hash = Hash.new {|h,k| h[k] = [] }
           @index = {}
         end
 
+        # Adds an acceptable item with the given priority to the list.
+        # @param [Float] q the priority
+        # @param [String] choice the acceptable item
         def add(q, choice)
           @index[choice] = q
           @hash[q] << choice
         end
 
+        # Given a raw acceptable value from an acceptance header,
+        # parse and add it to the list.
+        # @param [String] c the raw acceptable item
+        # @see #add
         def add_header_val(c)
           if c =~ CONNEG_REGEX
             choice, q = $1, $2
@@ -156,14 +197,25 @@ module Webmachine
           end
         end
 
+        # @param [Float] q the priority to lookup
+        # @return [Array<String>] the list of acceptable items at
+        #     the given priority
         def [](q)
           @hash[q]
         end
 
+        # @param [String] choice the acceptable item
+        # @return [Float] the priority of that value
         def priority_of(choice)
           @index[choice]
         end
 
+        # Iterates over the list in priority order, that is, taking
+        # into account the order in which items were added as well as
+        # their priorities.
+        # @yield [q,v]
+        # @yieldparam [Float] q the acceptable item's priority
+        # @yieldparam [String] v the acceptable item
         def each
           @hash.to_a.sort.reverse_each do |q,l|
             l.each {|v| yield q, v }
@@ -171,20 +223,24 @@ module Webmachine
         end
       end
 
+      # Like a {PriorityList}, but for {MediaTypes}, since they have
+      # parameters in addition to q.
       # @private
-      # Media types have parameters in addition to q, so we have to
-      # take those into account.
       class MediaTypeList < PriorityList
         include Translation
 
         MEDIA_TYPE_REGEX = /^\s*([^;]+)((?:;\S+\s*)*)\s*$/
         PARAMS_REGEX = /;([^=]+)=([^;=\s]+)/
+
+        # Overrides {PriorityList#add_header_val} to insert
+        # {MediaType} items instead of Strings.
+        # @see PriorityList#add_header_val
         def add_header_val(c)
           if c =~ MEDIA_TYPE_REGEX
             type, raw_params = $1, $2
             params = Hash[raw_params.scan(PARAMS_REGEX)]
             q = params.delete('q') || 1.0
-            add(q.to_f, [type, params])
+            add(q.to_f, MediaType.new(type, params))
           else
             raise MalformedRequest, t('invalid_media_type', :type => c)
           end
