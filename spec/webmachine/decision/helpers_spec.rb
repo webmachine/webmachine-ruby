3c6c5afcc04be17024f9c539107c5ab80ac1f258
Bernd Ahlers <bernd@tuneafish.de>
2011-09-02 17:16:28 +0200

    Check response headers for the different bodies.

--- spec/webmachine/decision/helpers_spec.rb
+++ spec/webmachine/decision/helpers_spec.rb
@@ -29,6 +29,23 @@ describe Webmachine::Decision::Helpers do
         subject.encode_body
         String.should === response.body
       end
+
+      it "sets the Content-Length header in the response" do
+        subject.encode_body
+        response.headers['Content-Length'].should == response.body.bytesize.to_s
+      end
+    end
+
+    shared_examples_for "a non-String body" do
+      it "does not set the Content-Length header in the response" do
+        subject.encode_body
+        response.headers.should_not have_key('Content-Length')
+      end
+
+      it "sets the Transfer-Encoding response header to chunked" do
+        subject.encode_body
+        response.headers['Transfer-Encoding'].should == 'chunked'
+      end
     end
 
     context "with an Enumerable body" do
@@ -38,6 +55,8 @@ describe Webmachine::Decision::Helpers do
         subject.encode_body
         Webmachine::EnumerableEncoder.should === response.body
       end
+
+      it_should_behave_like "a non-String body"
     end
 
     context "with a callable body" do
@@ -47,6 +66,8 @@ describe Webmachine::Decision::Helpers do
         subject.encode_body
         Webmachine::CallableEncoder.should === response.body
       end
+
+      it_should_behave_like "a non-String body"
     end
   end
 end
