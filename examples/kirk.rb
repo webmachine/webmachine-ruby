6c1d17c3e7391de4a34ab3c796a9d4e55233b7a4
Bernd Ahlers <bernd@tuneafish.de>
2011-09-02 14:41:03 +0200

    Add example for kirk.

---
+++ examples/kirk.rb
@@ -0,0 +1,21 @@
+require 'webmachine'
+require 'webmachine/adapters/kirk'
+
+class HelloResource < Webmachine::Resource
+  def last_modified
+    File.mtime(__FILE__)
+  end
+
+  def encodings_provided
+    { "gzip" => :encode_gzip, "identity" => :encode_identity }
+  end
+
+  def to_html
+    "<html><head><title>Hello from Webmachine</title></head><body>Hello, world!</body></html>"
+  end
+end
+
+Webmachine::Dispatcher.add_route([], HelloResource)
+
+Webmachine.adapter = :Kirk
+Webmachine.run
