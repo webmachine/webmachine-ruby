bca557a1b5c08d70382effaa3638a88591e38e7c
Ian Plosker <iman1003@gmail.com>
2011-09-10 14:16:52 -0400

    Autoload mongrel adapter Added Mongrel to gemfile

--- lib/webmachine/adapters.rb
+++ lib/webmachine/adapters.rb
@@ -1,9 +1,9 @@
 require 'webmachine/adapters/webrick'
-require 'webmachine/adapters/mongrel'
 
 module Webmachine
   # Contains classes and modules that connect Webmachine to Ruby
   # application servers.
   module Adapters
+    autoload :Mongrel, 'webmachine/adapters/mongrel'
   end
 end
