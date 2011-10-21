344bf4cc81063d7b3daeb0fdee04b9ea19be09c1
Sean Cribbs <sean@basho.com>
2011-10-21 14:06:50 -0400

    Move the documentation inside the module.

--- lib/webmachine/adapters/rack.rb
+++ lib/webmachine/adapters/rack.rb
@@ -5,25 +5,24 @@ require 'webmachine/request'
 require 'webmachine/response'
 require 'webmachine/dispatcher'
 
-# A minimal "shim" adapter to allow Webmachine to interface with Rack. The
-# intention here is to allow Webmachine to run under Rack-compatible
-# web-servers, like unicorn and pow, and is not intended to allow Webmachine
-# to be "plugged in" to an existin Rack app as middleware.
-#
-# To use this adapter, create a config.ru file and populate it like so:
-#
-#     require 'webmachine/adapters/rack'
-#
-#     # put your own Webmachine resources in another file:
-#     require 'my/resources'
-#
-#     run Webmachine::Adapters::Rack.new
-#
-# Servers like pow and unicorn will read config.ru by default and it should
-# all "just work".
-
 module Webmachine
   module Adapters
+    # A minimal "shim" adapter to allow Webmachine to interface with Rack. The
+    # intention here is to allow Webmachine to run under Rack-compatible
+    # web-servers, like unicorn and pow, and is not intended to allow Webmachine
+    # to be "plugged in" to an existin Rack app as middleware.
+    #
+    # To use this adapter, create a config.ru file and populate it like so:
+    #
+    #     require 'webmachine/adapters/rack'
+    #
+    #     # put your own Webmachine resources in another file:
+    #     require 'my/resources'
+    #
+    #     run Webmachine::Adapters::Rack.new
+    #
+    # Servers like pow and unicorn will read config.ru by default and it should
+    # all "just work".
     class Rack
       def call(env)
         headers = Webmachine::Headers.new
@@ -33,12 +32,12 @@ module Webmachine
             headers[key] = value
           end
         end
-        
+
         rack_req = ::Rack::Request.new env
         request = Webmachine::Request.new(rack_req.request_method,
-                    URI.parse(rack_req.url),
-                    headers,
-                    rack_req.body)
+                                          URI.parse(rack_req.url),
+                                          headers,
+                                          rack_req.body)
 
         response = Webmachine::Response.new
         Webmachine::Dispatcher.dispatch request, response
