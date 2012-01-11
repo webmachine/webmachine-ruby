67b9d973352728ebbf98ae9189754ea74b07db51
Bernerd Schaefer <bj.schaefer@gmail.com>
2012-01-11 23:16:11 +0800

    Skip mongrel adapter spec reliably across platforms
    `return` from main scope is a SyntaxError on 1.9.2.

--- spec/webmachine/adapters/mongrel_spec.rb
+++ spec/webmachine/adapters/mongrel_spec.rb
@@ -1,23 +1,22 @@
 require "spec_helper"
 
-if RUBY_PLATFORM =~ /java/
+begin
+  describe Webmachine::Adapters::Mongrel do
+    let(:configuration) { Webmachine::Configuration.default }
+    let(:dispatcher) { Webmachine::Dispatcher.new }
+
+    let(:adapter) do
+      described_class.new(configuration, dispatcher)
+    end
+
+    it "inherits from Webmachine::Adapter" do
+      adapter.should be_a_kind_of(Webmachine::Adapter)
+    end
+
+    it "implements #run" do
+      described_class.instance_methods(false).map {|m| m.to_sym }.should include :run
+    end
+  end
+rescue LoadError
   warn "Platform is #{RUBY_PLATFORM}: skipping mongrel adapter spec."
-  return
-end
-
-describe Webmachine::Adapters::Mongrel do
-  let(:configuration) { Webmachine::Configuration.default }
-  let(:dispatcher) { Webmachine::Dispatcher.new }
-
-  let(:adapter) do
-    described_class.new(configuration, dispatcher)
-  end
-
-  it "inherits from Webmachine::Adapter" do
-    adapter.should be_a_kind_of(Webmachine::Adapter)
-  end
-
-  it "implements #run" do
-    described_class.instance_methods(false).map {|m| m.to_sym }.should include :run
-  end
 end
