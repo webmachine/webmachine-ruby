689c420b283ce5988823157dd4c15b906120b783
Sam Goldman <samwgoldman@gmail.com>
2012-02-02 16:46:46 -0500

    Fix issue where CGI CONTENT_LENGTH header was not converted.

--- spec/webmachine/headers_spec.rb
+++ spec/webmachine/headers_spec.rb
@@ -6,6 +6,13 @@ describe Webmachine::Headers do
     subject['CONTENT-TYPE'].should == 'text/plain'
     subject.delete('CoNtEnT-tYpE').should == 'text/plain'
   end
+
+  describe "#from_cgi" do
+    it "should understand the Content-Length header" do
+      headers = described_class.from_cgi("CONTENT_LENGTH" => 14)
+      headers["content-length"].should == 14
+    end
+  end
   
   context "filtering with #grep" do
     subject { described_class["content-type" => "text/plain", "etag" => '"abcdef1234567890"'] }
