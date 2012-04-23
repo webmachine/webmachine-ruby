c085aa5f0cca9e853ca094c28d7e4783bee23de2
Sean Cribbs <sean@basho.com>
2012-04-22 20:52:39 -0400

    Add spec for trace stores.

---
+++ spec/webmachine/trace/trace_store_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'fileutils'
+
+shared_examples_for "trace storage" do
+  it { should respond_to(:[]=) }
+  it { should respond_to(:keys) }
+  it { should respond_to(:fetch) }
+
+  it "stores a trace" do
+    subject["foo"] = [:bar]
+    subject.fetch("foo").should == [:bar]
+  end
+
+  it "lists a stored trace in the keys" do
+    subject["foo"] = [:bar]
+    subject.keys.should == ["foo"]
+  end
+end
+
+describe Webmachine::Trace::PStoreTraceStore do
+  subject { described_class.new("./wmtrace") }
+  after { FileUtils.rm_rf("./wmtrace") }
+  it_behaves_like "trace storage"  
+end
+
+describe "Webmachine::Trace :memory Trace Store (Hash)" do
+  subject { Hash.new }
+  it_behaves_like "trace storage"
+end
