bec87bcd51f0736ab6c54b617cc01e202c06ad7e
Tony Arcieri <tony.arcieri@gmail.com>
2012-12-28 17:04:17 -0800

    Use the documentation formatter on Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ RSpec.configure do |config|
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.formatter = :documentation if ENV['CI']
   if defined?(::Java)
     config.seed = Time.now.utc
   else
