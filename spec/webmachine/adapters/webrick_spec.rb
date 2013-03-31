2e5691bb370173ec1b51e3b995cf3c786be6b547
Sam Goldman <samwgoldman@gmail.com>
2013-03-31 11:53:15 -1000

    Remove verbose debugging output

--- spec/webmachine/adapters/webrick_spec.rb
+++ spec/webmachine/adapters/webrick_spec.rb
@@ -3,8 +3,6 @@ require "support/adapter_lint"
 
 describe Webmachine::Adapters::WEBrick do
   it_should_behave_like :adapter_lint do
-    let(:adapter_options) { { :Logger => WEBrick::Log.new(STDERR, WEBrick::Log::DEBUG) } }
-
     it "should set Server header" do
       response = client.request(Net::HTTP::Get.new("/test"))
       response["Server"].should match(/Webmachine/)
