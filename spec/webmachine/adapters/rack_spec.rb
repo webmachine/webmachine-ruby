a8a868eda44733fe1e629c821f73fa8ce73c43f2
Paweł Pacana <pawel.pacana@gmail.com>
2013-07-18 00:05:56 +0200

    Don't rely on Rack::Response for sanitizing response headers.
    Rack::Response iterates over given body which in some cases,
    like Fiber streaming, may block responding to client.
    https://github.com/rack/rack/blob/master/lib/rack/response.rb#L35-L38
    
    RackResponse is introduced instead to cleanup Content-Type header
    in order to pass Rack::Lint.

--- spec/webmachine/adapters/rack_spec.rb
+++ spec/webmachine/adapters/rack_spec.rb
@@ -160,3 +160,25 @@ describe Webmachine::Adapters::Rack do
     last_response.body.split("\r\n").should == %W{6 Stream 0}
   end
 end
+
+describe Webmachine::Adapters::Rack::RackResponse do
+  context "on Rack < 1.5 release" do
+    before { Rack.stub(:release => "1.4") }
+
+    it "should add Content-Type header on not acceptable response" do
+      rack_response = described_class.new(double(:body), 406, {})
+      rack_status, rack_headers, rack_body = rack_response.finish
+      rack_headers.should have_key("Content-Type")
+    end
+  end
+
+  context "on Rack >= 1.5 release" do
+    before { Rack.stub(:release => "1.5") }
+
+    it "should not add Content-Type header on not acceptable response" do
+      rack_response = described_class.new(double(:body), 406, {})
+      rack_status, rack_headers, rack_body = rack_response.finish
+      rack_headers.should_not have_key("Content-Type")
+    end
+  end
+end
