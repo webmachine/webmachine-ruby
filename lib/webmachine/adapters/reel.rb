1e0d61e63ed5ab15f749df2587d9fad79dbf74da
Asmod4n <Asmod4n@users.noreply.github.com>
2014-01-23 12:13:48 +0100

    Update Reel Adapter for next Release
    @tarcieri Would this work?

--- lib/webmachine/adapters/reel.rb
+++ lib/webmachine/adapters/reel.rb
@@ -24,15 +24,25 @@ module Webmachine
           @extra_verbs = Set.new
         end
 
-        @server = ::Reel::Server.supervise(@options[:host], @options[:port], &method(:process))
+        @server = ::Reel::Server::HTTP.supervise(@options[:host], @options[:port], &method(:process))
+
+        if @options[:ssl] && @options[:ssl][:host] && @options[:ssl][:port] && @options[:ssl][:cert] && @options[:ssl][:key]
+          @ssl_server = ::Reel::Server::SSL.supervise(@options[:ssl][:host], @options[:ssl][:port], @options[:ssl], &method(:process))
+        end
 
         # FIXME: this will no longer work on Ruby 2.0. We need Celluloid.trap
-        trap("INT") { @server.terminate; exit 0 }
+        trap('INT') {
+          @server.terminate
+          @ssl_server.terminate if @ssl_server
+          exit 0
+        }
+
         Celluloid::Actor.join(@server)
       end
 
       def shutdown
         @server.terminate! if @server
+        @ssl_server.terminate! if @ssl_server
       end
 
       def process(connection)
