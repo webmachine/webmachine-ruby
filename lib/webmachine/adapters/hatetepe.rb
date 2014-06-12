webmachine/webmachine-ruby

commit 69be64ba4d0a034f4aa36f3f603d36cbea73aecf

Exposes the Webmachine::Application instance to the Adapter
Also randomly assignes a port when running RSpec

lib/webmachine/adapters/hatetepe.rb
@@ -16,8 +16,8 @@ module Webmachine
     class Hatetepe < Adapter
       def options
         {
-          :host => configuration.ip,
-          :port => configuration.port,
+          :host => application.configuration.ip,
+          :port => application.configuration.port,
           :app  => [
             ::Hatetepe::Server::Pipeline,
             ::Hatetepe::Server::KeepAlive,
@@ -40,7 +40,7 @@ module Webmachine
 
       def call(request, &respond)
         response = Webmachine::Response.new
-        dispatcher.dispatch(convert_request(request), response)
+        application.dispatcher.dispatch(convert_request(request), response)
 
         respond.call(convert_response(response))
       end

spec/webmachine/adapters/hatetepe_spec.rb
@@ -1,9 +1,14 @@
 require "spec_helper"
 
 examples = proc do
-  let(:configuration) { Webmachine::Configuration.default              }
-  let(:dispatcher)    { Webmachine::Dispatcher.new                     }
-  let(:adapter)       { described_class.new(configuration, dispatcher) }
+  let(:application) { Webmachine::Application.new }
+  let(:adapter) do
+    server = TCPServer.new('0.0.0.0', 0)
+    application.configuration.port = server.addr[1]
+    server.close
+
+    described_class.new(application)
+  end
 
   it "inherits from Webmachine::Adapter" do
     adapter.should be_a(Webmachine::Adapter)
@@ -22,7 +27,7 @@ examples = proc do
     end
 
     it "builds a string-like and enumerable request body" do
-      dispatcher.should_receive(:dispatch) do |req, res|
+      application.dispatcher.should_receive(:dispatch) do |req, res|
         req.body.to_s.should       eq("hello, world!")
         enum_to_s(req.body).should eq("hello, world!")
       end
@@ -31,7 +36,7 @@ examples = proc do
 
     shared_examples "enumerable response body" do
       before do
-        dispatcher.stub(:dispatch) {|_, response| response.body = body }
+        application.dispatcher.stub(:dispatch) {|_, response| response.body = body }
       end
 
       it "builds an enumerable response body" do

.gitignore
@@ -26,3 +26,5 @@ Gemfile.lock
 **/bin
 *.rbc
 .rvmrc
+.SyncID
+.SyncIgnore

.travis.yml
@@ -1,8 +1,15 @@
 rvm:
   - 1.9.3
   - 2.0.0
+  - 2.1.0
+  - ruby-head
   - jruby
-  - rbx
+  - jruby-head
+  - rbx-2
+
+matrix:
+  allow_failures:
+    - rvm: ruby-head
+    - rvm: jruby-head
+
 bundler_args: --without guard docs
-before_install:
-  - gem install bundler -v '= 1.5.1'

Gemfile
@@ -1,10 +1,16 @@
 require 'rbconfig'
-
 source 'https://rubygems.org'
-
 gemspec
 
-gem 'bundler'
+group :development do
+  gem "yard"
+  gem "rake"
+end
+
+group :test do
+  gem "rspec"
+  gem "rack"
+end
 
 group :webservers do
   gem 'mongrel',  '~> 1.2.beta', :platform => [:mri, :rbx]
@@ -33,9 +39,4 @@ end
 
 platforms :jruby do
   gem 'jruby-openssl'
-end
-
-platform :rbx do
-  gem 'rubysl'
-  gem 'racc'
 end

lib/webmachine/adapter.rb
@@ -5,23 +5,17 @@ module Webmachine
   # @abstract Subclass and override {#run} to implement a custom adapter.
   class Adapter
 
-    # @return [Webmachine::Configuration] the application's configuration.
-    attr_reader :configuration
+    # @return [Webmachine::Application] returns the application
+    attr_reader :application
 
-    # @return [Webmachine::Dispatcher] the application's dispatcher.
-    attr_reader :dispatcher
-
-    # @param [Webmachine::Configuration] configuration the application's
-    # configuration.
-    # @param [Webmachine::Dispatcher] dispatcher the application's dispatcher.
-    def initialize(configuration, dispatcher)
-      @configuration = configuration
-      @dispatcher    = dispatcher
+    # @param [Webmachine::Application] application the application
+    def initialize(application)
+      @application = application
     end
 
     # Create a new adapter and run it.
-    def self.run(configuration, dispatcher)
-      new(configuration, dispatcher).run
+    def self.run(application)
+      new(application).run
     end
 
     # Start the adapter.

lib/webmachine/adapters/mongrel.rb
@@ -14,13 +14,13 @@ module Webmachine
       # Starts the Mongrel adapter
       def run
         defaults = {
-          :port => configuration.port,
-          :host => configuration.ip,
-          :dispatcher => dispatcher
-        }.merge(configuration.adapter_options)
+          :port => application.configuration.port,
+          :host => application.configuration.ip,
+          :application => application
+        }.merge(application.configuration.adapter_options)
         @config = ::Mongrel::Configurator.new(defaults) do
           listener do
-            uri '/', :handler => Webmachine::Adapters::Mongrel::Handler.new(defaults[:dispatcher])
+            uri '/', :handler => Webmachine::Adapters::Mongrel::Handler.new(defaults[:application])
           end
           trap("INT") { stop }
           run
@@ -36,8 +36,8 @@ module Webmachine
 
       # A Mongrel handler for Webmachine
       class Handler < ::Mongrel::HttpHandler
-        def initialize(dispatcher)
-          @dispatcher = dispatcher
+        def initialize(application)
+          @application = application
           super()
         end
 
@@ -51,7 +51,7 @@ module Webmachine
                                             RequestBody.new(wreq))
 
           response = Webmachine::Response.new
-          @dispatcher.dispatch(request, response)
+          @application.dispatcher.dispatch(request, response)
 
           begin
             wres.status = response.code.to_i

lib/webmachine/adapters/rack.rb
@@ -38,9 +38,9 @@ module Webmachine
       def run
         options = DEFAULT_OPTIONS.merge({
           :app => self,
-          :Port => configuration.port,
-          :Host => configuration.ip
-        }).merge(configuration.adapter_options)
+          :Port => application.configuration.port,
+          :Host => application.configuration.ip
+        }).merge(application.configuration.adapter_options)
 
         @server = ::Rack::Server.new(options)
         @server.start
@@ -62,7 +62,7 @@ module Webmachine
                                           RequestBody.new(rack_req))
 
         response = Webmachine::Response.new
-        @dispatcher.dispatch(request, response)
+        application.dispatcher.dispatch(request, response)
 
         response.headers['Server'] = [Webmachine::SERVER_STRING, "Rack/#{::Rack.version}"].join(" ")
 

commit 57834333887b99dc7e765fdcc036436f39a709b2

Fix Hatetepe adapter and spec, broken by 69be64b and 17a9819

lib/webmachine/adapters/hatetepe.rb
@@ -86,8 +86,8 @@ module Webmachine
         uri.scheme = "http"
 
         host     = request.headers.fetch("Host", "").split(":")
-        uri.host = host[0]      || configuration.ip
-        uri.port = host[1].to_i || configuration.port
+        uri.host = host[0]      || application.configuration.ip
+        uri.port = host[1].to_i || application.configuration.port
 
         URI.parse(uri.to_s)
       end

spec/webmachine/adapters/hatetepe_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-examples = proc do
+describe Webmachine::Adapters::Hatetepe do
   let(:application) { Webmachine::Application.new }
   let(:adapter) do
     server = TCPServer.new('0.0.0.0', 0)