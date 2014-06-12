8660e30d0e378e29e953402a95f78ca48d16cf3e
Sean Cribbs <sean@basho.com>
2014-06-12 10:54:53 -0400

    Resolve deprecation warnings in events_spec

--- spec/webmachine/events_spec.rb
+++ spec/webmachine/events_spec.rb
@@ -3,20 +3,20 @@ require 'spec_helper'
 describe Webmachine::Events do
   describe ".backend" do
     it "defaults to AS::Notifications" do
-      described_class.backend.should be(AS::Notifications)
+      expect(described_class.backend).to be(AS::Notifications)
     end
   end
 
   describe ".publish" do
     it "calls the backend" do
-      described_class.backend.should_receive(:publish).with('test.event', 1, 'two')
+      expect(described_class.backend).to receive(:publish).with('test.event', 1, 'two')
       described_class.publish('test.event', 1, 'two')
     end
   end
 
   describe ".instrument" do
     it "calls the backend" do
-      described_class.backend.should_receive(:instrument).with(
+      expect(described_class.backend).to receive(:instrument).with(
         'test.event', {}
       ).and_yield
 
@@ -26,7 +26,7 @@ describe Webmachine::Events do
 
   describe ".subscribe" do
     it "calls the backend" do
-      described_class.backend.should_receive(:subscribe).with(
+      expect(described_class.backend).to receive(:subscribe).with(
         'test.event'
       ).and_yield
 
@@ -38,7 +38,7 @@ describe Webmachine::Events do
     it "calls the backend" do
       callback = Proc.new { }
 
-      described_class.backend.should_receive(:subscribed).with(
+      expect(described_class.backend).to receive(:subscribed).with(
         callback, 'test.event'
       ).and_yield
 
@@ -50,7 +50,7 @@ describe Webmachine::Events do
     it "calls the backend" do
       subscriber = described_class.subscribe('test.event') { }
 
-      described_class.backend.should_receive(:unsubscribe).with(subscriber)
+      expect(described_class.backend).to receive(:unsubscribe).with(subscriber)
 
       described_class.unsubscribe(subscriber)
     end
