cef18d64df82fe75412250ce6006dee68d08bd23
Sean Cribbs <sean@basho.com>
2014-06-12 09:59:49 -0400

    Fix failing specs and deprecations in response_spec.

--- spec/webmachine/response_spec.rb
+++ spec/webmachine/response_spec.rb
@@ -3,20 +3,20 @@ require 'spec_helper'
 describe Webmachine::Response do
 
   it "should have sane default values" do
-    subject.code.should == 200
-    subject.is_redirect?.should be(false)
-    subject.headers.should be_empty
+    expect(subject.code).to eq(200)
+    expect(subject.is_redirect?).to be(false)
+    expect(subject.headers).to be_empty
   end
 
   describe "a redirected response" do
     let(:redirect_url) { "/" }
 
-    before(:all) { subject.redirect_to redirect_url }
+    before(:each) { subject.redirect_to redirect_url }
 
     its(:is_redirect?) { should be(true) }
 
     it "should have a proper Location header" do
-      subject.headers["Location"].should == redirect_url
+      expect(subject.headers["Location"]).to eq(redirect_url)
     end
   end
 
@@ -24,21 +24,21 @@ describe Webmachine::Response do
     let(:cookie) { "monster" }
     let(:cookie_value) { "mash" }
 
-    before(:all) { subject.set_cookie(cookie, cookie_value) }
+    before(:each) { subject.set_cookie(cookie, cookie_value) }
 
     it "should have a proper Set-Cookie header" do
-      subject.headers["Set-Cookie"].should include "monster=mash";
+      expect(subject.headers["Set-Cookie"]).to include "monster=mash"
     end
 
     describe "setting multiple cookies" do
       let(:cookie2) { "rodeo" }
       let(:cookie2_value) { "clown" }
-      before(:all) { subject.set_cookie(cookie2, cookie2_value) }
+      before(:each) { subject.set_cookie(cookie2, cookie2_value) }
 
       it "should have a proper Set-Cookie header" do
-        subject.headers["Set-Cookie"].should be_a Array
-        subject.headers["Set-Cookie"].should include "rodeo=clown"
-        subject.headers["Set-Cookie"].should include "monster=mash"
+        expect(subject.headers["Set-Cookie"]).to be_a Array
+        expect(subject.headers["Set-Cookie"]).to include "rodeo=clown"
+        expect(subject.headers["Set-Cookie"]).to include "monster=mash"
       end
     end
   end
