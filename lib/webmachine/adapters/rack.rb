87ebe0254d3827923583765c2d446252cc75e2bc
Beth <beth@bethesque.com>
2015-01-04 10:33:19 +1100

    Oops! Use env not ENV.

--- lib/webmachine/adapters/rack.rb
+++ lib/webmachine/adapters/rack.rb
@@ -60,7 +60,7 @@ module Webmachine
 
         rack_req = ::Rack::Request.new env
         request = Webmachine::Request.new(rack_req.request_method,
-                                          ENV[REQUEST_URI] || rack_req.url,
+                                          env[REQUEST_URI] || rack_req.url,
                                           headers,
                                           RequestBody.new(rack_req))
 
