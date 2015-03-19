940a0b03f390125ea729cb101c09f8e3b3803b59
Beth <beth@bethesque.com>
2015-03-20 10:12:29 +1100

    Moved RackMapped class into its own file.

--- lib/webmachine/adapters/rack.rb
+++ lib/webmachine/adapters/rack.rb
@@ -186,41 +186,5 @@ module Webmachine
         end
       end # class RequestBody
     end # class Rack
-
-    # Provides the same functionality as the parent Webmachine::Adapters::Rack
-    # adapter, but allows the Webmachine application to be hosted at an
-    # arbitrary path in a parent Rack application (as in Rack `map` or Rails
-    # routing `mount`)
-    #
-    # This functionality is separated out from the parent class to preserve
-    # backward compatibility in the behaviour of the parent Rack adpater.
-    #
-    # To use the adapter in a parent Rack application, map the Webmachine
-    # application as follows in a rackup file or Rack::Builder:
-    #
-    #   map '/foo' do
-    #     run SomeotherRackApp
-    #
-    #     map '/bar' do
-    #       run MyWebmachineApp.adapter
-    #     end
-    #   end
-    class RackMapped < Rack
-      protected
-      def routing_tokens(rack_req)
-        routing_match = rack_req.path_info.match(Webmachine::Request::ROUTING_PATH_MATCH)
-        routing_path = routing_match ? routing_match[1] : ""
-        routing_path.split(SLASH)
-      end
-
-      def base_uri(rack_req)
-        # rack SCRIPT_NAME env var doesn't end with "/". This causes weird
-        # behavour when URI.join concatenates URI components in
-        # Webmachine::Decision::Flow#n11
-        script_name = rack_req.script_name + SLASH
-        URI.join(rack_req.base_url, script_name)
-      end
-    end
-
   end # module Adapters
 end # module Webmachine
--- spec/webmachine/adapters/rack_spec.rb
+++ spec/webmachine/adapters/rack_spec.rb
@@ -14,16 +14,6 @@ describe Webmachine::Adapters::Rack do
   end
 end
 
-describe Webmachine::Adapters::RackMapped do
-  it_should_behave_like :adapter_lint do
-    it "should set Server header" do
-      response = client.request(Net::HTTP::Get.new("/test"))
-      expect(response["Server"]).to match(/Webmachine/)
-      expect(response["Server"]).to match(/Rack/)
-    end
-  end
-end
-
 describe Webmachine::Adapters::Rack::RackResponse do
   context "on Rack < 1.5 release" do
     before { allow(Rack).to receive_messages(:release => "1.4") }
@@ -62,62 +52,6 @@ describe Webmachine::Adapters::Rack do
     it "provides the full request URI" do
       rack_response = get "test", nil, {"HTTP_ACCEPT" => "test/response.request_uri"}
       expect(rack_response.body).to eq "http://example.org/test"
-    end
-  end
-end
-
-describe Webmachine::Adapters::RackMapped do
-  class CreateResource < Webmachine::Resource
-    def allowed_methods
-      ["POST"]
-    end
-
-    def content_types_accepted
-      [["application/json", :from_json]]
-    end
-
-    def content_types_provided
-      [["application/json", :to_json]]
-    end
-
-    def post_is_create?
-      true
-    end
-
-    def create_path
-      "created_path_here/123"
-    end
-
-    def from_json
-      response.body = %{ {"foo": "bar"} }
-    end
-  end
-
-  let(:app) do
-    Rack::Builder.new do
-      map '/some/route' do
-        run(Webmachine::Application.new do |app|
-          app.add_route(["test"], Test::Resource)
-          app.add_route(["create_test"], CreateResource)
-          app.configure do | config |
-            config.adapter = :RackMapped
-          end
-        end.adapter)
-      end
-    end
-  end
-
-  context "using Rack::Test" do
-    include Rack::Test::Methods
-
-    it "provides the full request URI" do
-      rack_response = get "some/route/test", nil, {"HTTP_ACCEPT" => "test/response.request_uri"}
-      expect(rack_response.body).to eq "http://example.org/some/route/test"
-    end
-
-    it "provides LOCATION header using custom base_uri when creating from POST request" do
-      rack_response = post "/some/route/create_test", %{{"foo": "bar"}}, {"HTTP_ACCEPT" => "application/json", "CONTENT_TYPE" => "application/json"}
-      expect(rack_response.headers["Location"]).to eq("http://example.org/some/route/created_path_here/123")
     end
   end
 end
